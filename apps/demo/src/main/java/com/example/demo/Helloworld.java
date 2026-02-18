1c9cbb86ea98df4c4c76a8464c1f8480de55e11c
shota-sato <bv13025@gmail.com>
2026-02-18 22:36:16 +0900

    demo修正

--- apps/demo/src/main/java/com/example/demo/Helloworld.java
+++ apps/demo/src/main/java/com/example/demo/Helloworld.java
@@ -4,16 +4,13 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class Helloworld {
 
-    @RestController
-    public class HelloController {
-
-        @GetMapping("/hello")
-        public String hello() {
-            return "Hello World!";
-        }
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
     }
-
-
 }
+
+
